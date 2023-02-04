9717f731dc7db680c71bedbc6db03bfb2686a21a
su_song <su_song@glifemail.com>
2023-02-04 17:34:15 +0800

    使用fastjson

--- mflog/src/main/java/com/mf/log/upload/UploadFileToMec.java
+++ mflog/src/main/java/com/mf/log/upload/UploadFileToMec.java
@@ -2,7 +2,7 @@ package com.mf.log.upload;
 
 import androidx.annotation.NonNull;
 
-import com.alibaba.fastjson2.JSON;
+import com.alibaba.fastjson.JSON;
 import com.mf.log.LogUtils;
 import com.mf.log.upload.entity.ResultVo;
 
--- mflog/src/main/java/com/mf/log/upload/UploadFileToQiniu.java
+++ mflog/src/main/java/com/mf/log/upload/UploadFileToQiniu.java
@@ -2,8 +2,8 @@ package com.mf.log.upload;
 
 import androidx.annotation.NonNull;
 
-import com.alibaba.fastjson2.JSON;
-import com.alibaba.fastjson2.TypeReference;
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.TypeReference;
 import com.mf.log.LogUtils;
 import com.mf.log.upload.entity.ResultVo;
 import com.mf.log.upload.entity.UploadTokenRespVo;
